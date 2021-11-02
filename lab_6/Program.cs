c0440172dabc91487b3fc2a84a102790cf15ca71
PI03-Gavrilov <rol225rf.ru@mail.ru>
2021-11-02 13:29:30 +0700

    Editing class flat & main class for C#

--- lab_6/Program.cs
+++ lab_6/Program.cs
@@ -1,12 +1,254 @@
 ﻿using System;
 
 namespace lab_6
+
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+			//Подключение Русского языка
+			//SetConsoleCP(1251);
+			//SetConsoleOutputCP(1251);
+
+			int option = 0;
+
+			do
+			{
+				 Console.WriteLine("  1) Структура location\n  2) Структура flat\n  3) Структура street\n  4) Структура house\n  5) Выход\n");
+				 Console.WriteLine("Выберите действие: ");
+				do
+				{
+					while (!int.TryParse(Console.ReadLine(), out option)) { Console.WriteLine("Ошибка. Введите число: "); }
+					if (option > 5 || option <=0)
+					{
+						Console.WriteLine("\nОшибка. выбирете из допустимых значений: ");
+					}
+				} while (option > 5 || option <= 0);
+				
+				if (option == 1)
+				{
+					//FunckForLocation();
+				}
+				else if (option == 2)
+				{
+					FunckForFlat();
+				}
+				else if (option == 3)
+				{
+					//FunckForStreet();
+				}
+				else if (option == 4)
+				{
+					//FunckForHouse();
+				}
+			} while (option != 5);
+
+			/*void FunckForLocation()
+			{
+
+				int distanceSchool, distanceHospital, distanceKindergarten, numHouse;
+				string houseStreet;
+				int option = 0;
+
+				location location_1 = new location();
+
+				do
+				{
+					Console.WriteLine("  1) Заполнить через встроенную функцию\n  2) Заполнить через внешнюю функцию\n  3) Вывести информацию со структуры\n  4) Выход в главное меню\n");
+					Console.WriteLine("Выберите действие: ");
+					do
+					{
+						while (!int.TryParse(Console.ReadLine(), out option)) { Console.WriteLine("Ошибка. Введите число: "); }
+						if (option > 4)
+						{
+							Console.WriteLine("\nОшибка. выбирете из допустимых значений: ");
+						}
+					} while (option > 4 || option <= 0);
+
+					if (option == 1)
+					{
+						location_1.Set_console();
+					}
+					else if (option == 2)
+					{
+						Console.WriteLine("\nВыберите расстояние до школы: ");
+						while (!int.TryParse(Console.ReadLine(), out distanceSchool)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("Выберите расстояние до больницы: ");
+						while (!int.TryParse(Console.ReadLine(), out distanceHospital)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("Выберите расстояние до детского сада: ");
+						while (!int.TryParse(Console.ReadLine(), out distanceKindergarten)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("Номер дома: ");
+						while (!int.TryParse(Console.ReadLine(), out numHouse)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("\nУлица: "); houseStreet = Console.ReadLine();
+
+						location_1.Set(distanceSchool, distanceHospital, distanceKindergarten, houseStreet, numHouse);
+					}
+					else if (option == 3)
+					{
+						Console.WriteLine("\n");
+						location_1.Print();
+					}
+				} while (option != 4);
+
+			}
+			*/
+			void FunckForFlat()
+			{
+				int coin, countRoom, numFlat;
+				int option = 0;
+
+				Flat flat_1 = new Flat();
+
+				do
+				{
+					Console.WriteLine("  1) Заполнить через встроенную функцию\n  2) Заполнить через внешнюю функцию\n  3) Вывести информацию со структуры\n  4) Выход в главное меню\n");
+					Console.WriteLine("Выберите действие: ");
+					do
+					{
+						while (!int.TryParse(Console.ReadLine(), out option)) { Console.WriteLine("Ошибка. Введите число: "); }
+						if (option > 4)
+						{
+							Console.WriteLine("\nОшибка. выбирете из допустимых значений: ");
+						}
+					} while (option > 4 || option <= 0);
+
+					if (option == 1)
+					{
+						flat_1.Set_console();
+					}
+					else if (option == 2)
+					{
+						Console.WriteLine("\nВыберите стоимость квартиры: ");
+						while (!int.TryParse(Console.ReadLine(), out coin)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("Выберите кол-во комнат: ");
+						while (!int.TryParse(Console.ReadLine(), out countRoom)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("Номер квартиры: ");
+						while (!int.TryParse(Console.ReadLine(), out numFlat)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						flat_1.Set(coin, countRoom, numFlat);
+					}
+					else if (option == 3)
+					{
+						flat_1.Print();
+					}
+
+				} while (option != 4);
+
+			}
+			/*
+			void FunckForStreet()
+			{
+				string streetName;
+				string streetDescription;
+
+				street street_1 = new street();
+
+				int option = 0;
+				do
+				{
+					Console.WriteLine("  1) Заполнить через встроенную функцию\n  2) Заполнить через внешнюю функцию\n  3) Вывести информацию со структуры\n  4) Выход в главное меню\n");
+					Console.WriteLine("Выберите действие: ");
+					do
+					{
+						while (!int.TryParse(Console.ReadLine(), out option)) { Console.WriteLine("Ошибка. Введите число: "); }
+						if (option > 4)
+						{
+							Console.WriteLine("\nОшибка. выбирете из допустимых значений: ");
+						}
+					} while (option > 4 || option <= 0);
+
+					if (option == 1)
+					{
+						street_1.Set_console();
+					}
+					else if (option == 2)
+					{
+						Console.WriteLine("\nУлица: ");
+						streetName = Console.ReadLine();
+						Console.WriteLine("Описание улици: ");
+						streetDescription = Console.ReadLine();
+
+						street_1.Set(streetName, streetDescription);
+					}
+					else if (option == 3)
+					{
+						street_1.Print();
+					}
+
+				} while (option != 4);
+
+			}
+
+			void FunckForHouse()
+			{
+				int distanceSchool, distanceHospital, distanceKindergarten, numHouse, coin, countRoom, numFlat;
+				string streetDescription, houseStreet;
+
+				house house_1 = new house();
+
+				int option = 0;
+				do
+				{
+					Console.WriteLine("  1) Заполнить через встроенную функцию\n  2) Заполнить через внешнюю функцию\n  3) Вывести информацию со структуры\n  4) Выход в главное меню\n");
+					Console.WriteLine("Выберите действие: ");
+					do
+					{
+						while (!int.TryParse(Console.ReadLine(), out option)) { Console.WriteLine("Ошибка. Введите число: "); }
+						if (option > 4)
+						{
+							Console.WriteLine("\nОшибка. выбирете из допустимых значений: ");
+						}
+					} while (option > 4 || option <= 0);
+
+					if (option == 1)
+					{
+						house_1.Set_console();
+					}
+					else if (option == 2)
+					{
+
+						Console.WriteLine("Выберите расстояние до школы: ");
+						while (!int.TryParse(Console.ReadLine(), out distanceSchool)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("Выберите расстояние до больницы: ");
+						while (!int.TryParse(Console.ReadLine(), out distanceHospital)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("Выберите расстояние до детского сада: ");
+						while (!int.TryParse(Console.ReadLine(), out distanceKindergarten)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("Выберите стоимость квартиры: ");
+						while (!int.TryParse(Console.ReadLine(), out coin)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("Выберите кол-во комнат: ");
+						while (!int.TryParse(Console.ReadLine(), out countRoom)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("Номер дома: ");
+						while (!int.TryParse(Console.ReadLine(), out numHouse)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("Номер квартиры: ");
+						while (!int.TryParse(Console.ReadLine(), out numFlat)) { Console.WriteLine("Ошибка. Введите число: "); }
+
+						Console.WriteLine("\nУлица: "); houseStreet = Console.ReadLine();
+						Console.WriteLine("Описание улици: "); streetDescription = Console.ReadLine();
+
+						house_1.Set(distanceSchool, distanceHospital, distanceKindergarten, houseStreet, numHouse, coin, countRoom, numFlat, streetDescription);
+					}
+					else if (option == 3)
+					{
+						house_1.Print();
+					}
+
+				} while (option != 4);
+
+			}
+			*/
+		}	
+	}
 }
---
+++ lab_6/flat.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_6
+{
+	class Flat
+	{
+		private int coin; //цена
+		private int countRoom; //кол-во комнат
+		private int numFlat; //Номер квартиры
+
+		public void Set(int coin_2, int countRoom_2, int numFlat_2) {
+			if (coin_2 <= 0)
+			{
+				coin = 0;
+			}
+			else
+			{
+				coin = coin_2;
+			}
+			if (countRoom_2 <= 0)
+			{
+				countRoom = 0;
+			}
+			else
+			{
+				countRoom = countRoom_2;
+			}
+			if (numFlat_2 <= 0)
+			{
+				numFlat = 0;
+			}
+			else
+			{
+				numFlat = numFlat_2;
+			}
+		} // Функция ввода через внешнюю функцию
+		public void Set_console() {
+			Console.WriteLine("\nВыберите стоимость квартиры: ");
+			do
+			{
+				while (!int.TryParse(Console.ReadLine(), out coin)) { Console.WriteLine("Ошибка. Введите число: "); }
+			} while (coin <= 0);
+
+			Console.WriteLine("Выберите кол-во комнат: ");
+			do
+			{
+				while (!int.TryParse(Console.ReadLine(), out countRoom)) { Console.WriteLine("Ошибка. Введите число: "); }
+			} while (countRoom <= 0);
+
+			Console.WriteLine("Номер квартиры: ");
+			do
+			{
+				while (!int.TryParse(Console.ReadLine(), out numFlat)) { Console.WriteLine("Ошибка. Введите число: "); }
+			} while (numFlat <= 0);
+		}  // Функция ввода через встроенную функцию
+		public void Print() {
+			Console.WriteLine("\n  Цена: {0}", coin);
+			Console.WriteLine("  Количество комнат: {0}", countRoom);
+			Console.WriteLine("  Номер квартиры: {0}\n", numFlat);
+		} // Функция вывода
+
+		/// Функции вывода каждой отдельной переменной
+		public int Get_coin() { return coin; }
+		public int Get_count_room() { return countRoom; }
+		public int Get_num_flat() { return numFlat; }
+	}
+
+}
